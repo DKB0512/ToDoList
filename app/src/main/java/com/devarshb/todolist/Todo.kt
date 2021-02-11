2a67a6e65bd44b8b133bd6eb4a750235307b5a1d
DKB007 <bhattdevarsh1.db@gmail.com>
2021-02-11 18:58:07 +0530

    Initial Commit

---
+++ app/src/main/java/com/devarshb/todolist/Todo.kt
@@ -0,0 +1,6 @@
+package com.devarshb.todolist
+
+data class Todo(
+    val title: String,
+    val isChecked: Boolean = false
+)
---
+++ app/src/main/java/com/devarshb/todolist/TodoAdapter.kt
@@ -0,0 +1,36 @@
+package com.devarshb.todolist
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.item_todo.view.*
+
+class TodoAdapter(
+    private val todos: MutableList<Todo>
+) : RecyclerView.Adapter<TodoAdapter.TodoViewHolder>() {
+
+    class TodoViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoViewHolder {
+        return TodoViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_todo,
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: TodoViewHolder, position: Int) {
+        val curTodo = todos[position]
+        holder.itemView.apply {
+            tvTodoTitle.text = curTodo.title
+            cbDone.isChecked = curTodo.isChecked
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return todos.size
+    }
+}
