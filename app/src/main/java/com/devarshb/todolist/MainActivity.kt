567f53bf49bbc382a642426cd121e07223260ecd
DKB007 <bhattdevarsh1.db@gmail.com>
2021-02-13 18:30:22 +0530

    Initial Commit

--- app/src/main/java/com/devarshb/todolist/MainActivity.kt
+++ app/src/main/java/com/devarshb/todolist/MainActivity.kt
@@ -22,6 +22,7 @@ class MainActivity : AppCompatActivity() {
             if(todoTitle.isNotEmpty()) {
                 val todo = Todo(todoTitle)
                 todoAdapter.addTodo(todo)
+                etTodoTitle.text.clear()
             }
         }
     }
